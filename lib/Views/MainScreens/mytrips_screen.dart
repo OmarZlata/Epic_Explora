8ff2d75dce60d352c503789f5a1bfc7dffa52541
Omar Zalata <omarzalata2002@gmail.com>
2024-03-03 12:11:33 +0200

    Initial commit

--- lib/Views/MainScreens/mytrips_screen.dart
+++ lib/Views/MainScreens/mytrips_screen.dart
@@ -9,37 +9,7 @@ class MyTripsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.white,
-          elevation: 1,
-          leading: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: AppColors.gray, width: 1),
-              ),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                child: Icon(
-                  Icons.arrow_back_ios_outlined,
-                  color: AppColors.gray,
-                ),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => bottomNavigationBar(),));
-                },
-              ),
-            ),
-          ),
-          title: Text(
-            "    My trips",
-            style: TextStyle(
-              color: AppColors.Black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-        ),
+
         body:Center(child: Text("My Trips"))
     );
   }
