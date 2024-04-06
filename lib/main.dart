54cc80a88d214c64201bb795fcde1d81836b5d1a
AbdelrhmanHesham2002 <abd302233@gmail.com>
2024-04-06 03:16:22 +0200

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,14 @@ import 'package:graduation_project/Views/Place_Detials/reviews_place_dwtials.dar
 import 'package:graduation_project/Views/Signin/Signin.dart';
 import 'package:graduation_project/Views/WelcomeScreens/splash_screen.dart';
 import 'package:graduation_project/core/app_colors/app_colors.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'Views/My_Trips/previous.dart';
 import 'Views/Signin/Forget_Password.dart';
 import 'Views/Signin/Reset_Password.dart';
 import 'Views/Signin/SignUp.dart';
 import 'Views/Signin/Verification.dart';
+import 'core/local/app_local.dart';
 void main() {
   runApp(MyApp());
 }
@@ -20,7 +22,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(localizationsDelegates: const[
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      AppLocalizations.delegate
+    ],
+      supportedLocales: const [
+        Locale('ar', "EG"),
+        Locale('en', "US"),
+      ],
+      locale: const Locale('ar'),
       title: 'Splash Screen',
       theme: ThemeData(
       ),
