e3db3df750d6f3a6138ab3b8603b2ca1318f9f58
Omar Zalata <omarzalata2002@gmail.com>
2024-04-24 21:53:59 +0200

    Some edits "signUpII"

--- lib/Views/WelcomeScreens/splash_screen.dart
+++ lib/Views/WelcomeScreens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart'; // Import for AnimationController
 import '../../core/app_colors/app_colors.dart';
 import 'onboarding_1.dart';
 
@@ -7,44 +8,79 @@ class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen> {
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                    WelcomeScreen1()
-            )
-        )
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2), // Adjust duration as needed
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    // Start animation
+    _controller.forward();
+
+    Timer(
+      Duration(seconds: 3),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WelcomeScreen1(),
+        ),
+      ),
     );
   }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose the controller when not needed
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.blue,
-        body  : Center(
-          child: Container(
-            height: 150,
-            width: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              color: Colors.white
-            ),
-              child: Column(
-
-                children: [
-                  SizedBox(height: 12,),
-                  Image.asset('assets/images/logo.png'),
-                  SizedBox(height: 10,),
-                  Text("Epic Explore",style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.black
-                  ),)
-                ],
-              )
+        body: Center(
+          child: AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return Transform.scale(
+                scale: _animation.value, // Apply scale animation
+                child: Opacity(
+                  opacity: _animation.value, // Apply opacity animation
+                  child: Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 12),
+                        Image.asset('assets/images/logo.png'),
+                        SizedBox(height: 10),
+                        Text(
+                          "Epic Explore",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
