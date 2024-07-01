81701a113c718c317a84b2f7394bf8cc2df7c521
Omar Ashraf Zalata <omarzalata2002@gmail.com>
2024-07-01 23:35:05 +0300

    search done

--- lib/Views/Home/360/screens/home_screen.dart
+++ lib/Views/Home/360/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:epic_expolre/Views/Home/360/components/thumbnail.dart';
+import 'package:epic_expolre/Widgets/app_AppBar.dart';
+import 'package:epic_expolre/core/app_colors/app_colors.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 
@@ -14,46 +16,56 @@ class HomeImageScreen extends StatelessWidget {
     final List<Thumbnail> thumbList = [
       const Thumbnail(
         imagePath: 'assets/images/balcony_water.jpg',
-        thumbPath: 'assets/images/thumbs/balcony_water_thumb.webp',
+        thumbPath: 'assets/images/balcony_water.jpg',
       ),
       const Thumbnail(
           imagePath: 'assets/images/boat.jpg',
-          thumbPath: 'assets/images/thumbs/boat_thumb.webp'),
+          thumbPath: 'assets/images/boat.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/carlsbad_nps.jpg',
-          thumbPath: 'assets/images/thumbs/carlsbad_nps_thumb.webp'),
+          thumbPath: 'assets/images/carlsbad_nps.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/carlsbad.jpg',
-          thumbPath: 'assets/images/thumbs/carlsbad_thumb.jpg'),
+          thumbPath: 'assets/images/carlsbad.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/dock.jpg',
-          thumbPath: 'assets/images/thumbs/dock_thumb.webp'),
+          thumbPath: 'assets/images/dock.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/game_world.jpeg',
-          thumbPath: 'assets/images/thumbs/game_world_thumb.webp'),
+          thumbPath: 'assets/images/game_world.jpeg'),
       const Thumbnail(
           imagePath: 'assets/images/lagoon.jpg',
-          thumbPath: 'assets/images/thumbs/lagoon_thumb.webp'),
+          thumbPath: 'assets/images/lagoon.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/mountain.jpg',
-          thumbPath: 'assets/images/thumbs/mountain_thumb.webp'),
+          thumbPath: 'assets/images/mountain.jpg'),
       const Thumbnail(
           imagePath: 'assets/images/planet.jpeg',
-          thumbPath: 'assets/images/thumbs/planet_thumb.webp'),
+          thumbPath: 'assets/images/planet.jpeg'),
       const Thumbnail(
           imagePath: 'assets/images/room.jpeg',
-          thumbPath: 'assets/images/thumbs/room_thumb.webp'),
-      const Thumbnail(
-          imagePath: 'assets/images/trees.jpeg',
-          thumbPath: 'assets/images/thumbs/trees_thumb.webp'),
+          thumbPath: 'assets/images/room.jpeg'),
       const Thumbnail(
           imagePath: 'assets/images/bunker.jpeg',
-          thumbPath: 'assets/images/thumbs/bunker_thumb.webp'),
+          thumbPath: 'assets/images/bunker.jpeg'),
       const Thumbnail(
           imagePath: 'assets/images/grocery.jpeg',
-          thumbPath: 'assets/images/thumbs/grocery_thumb.webp'),
+          thumbPath: 'assets/images/grocery.jpeg'),
     ];
     return Scaffold(
+      appBar: AppAppBar(
+        title: "360 degree Photo",
+        color: AppColors.blue,
+        textColor: AppColors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,color: AppColors.white,
+          ),
+        ),
+      ),
       body: SafeArea(
         child: kIsWeb
             ? Padding(
--- lib/Views/Home/360/screens/image_screen.dart
+++ lib/Views/Home/360/screens/image_screen.dart
@@ -1,3 +1,5 @@
+import 'package:epic_expolre/Widgets/app_text.dart';
+import 'package:epic_expolre/core/app_colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:panorama_viewer/panorama_viewer.dart';
@@ -9,7 +11,24 @@ class ImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new,color: AppColors.white,
+          )
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.blue,
+        title: AppText(
+          title: "360 Degree Photos",
+          color: AppColors.white,
+          fontSize: 18,
+
+        ),
+      ),
       body: PanoramaViewer(
         child: image,
       ),
